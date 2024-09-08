cb2f03d3d15a56b95d4441b65e38303393c0c83f
ale7516 <vanessaalejandrbarreramartin@gmail.com>
2024-09-07 23:29:44 -0500

    Ejercicios Java realizados
    8 ejercicios

--- src/main/java/ejercicios/Division.java
+++ src/main/java/ejercicios/Division.java
@@ -1,4 +1,3 @@
-
 package ejercicios;
 
 import static ejercicios.SetDeTenis.evaluar;
@@ -10,13 +9,17 @@ import java.util.Scanner;
  */
 public class Division {
     public static String evaluar(int dividendo, int divisor) {
-        // TODO: Coloca aquí el código del ejercicio 3: Division
-        int cociente = 0;
-        int residuo = 0;
-        String respuesta = "La división es exacta. \n"
+        int cociente = dividendo / divisor;
+        int residuo = dividendo % divisor;
+        if (residuo == 0) {
+            return  "La división es exacta. \n"
                 + "Cociente: " + cociente + "\n"
                 + "Residuo: " + residuo;
-        return respuesta;
+        } else {
+            return "La división no es exacta. \n"
+                + "Cociente: " + cociente + "\n"
+                + "Residuo: " + residuo;
+        }
     }
     
     public static void main(String[] args) {
